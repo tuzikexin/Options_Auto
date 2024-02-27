ff1717ce9afdca6b7f5b11c14038b98384d41fa8
student125 <student125_9e556a7569414119@code.jdcloud.com>
2024-02-27 13:39:39 +0100

    fix docker file

--- app/build_docker.sh
+++ app/build_docker.sh
@@ -23,24 +23,24 @@ if [ $? -eq 0 ]; then
     credentials_folder="$parent_dir/$credentials_f"
     echo 'credentials file path:' $credentials_folder
 
-    if [ -f $credentials_folder ]; then
+    if [ $(ls -A $credentials_folder | wc -l) -eq 0 ]; then
+        echo "credentials directory is empty."
+    else
         docker run --rm -v $credentials_folder:/app/credentials/ --name temp-vix-container tuzikexin/${IMAGE_NAME}:${IMAGE_TAG} --ticker VIX --end_time_h 23 --end_time_m 59 --test_mode yes
-    else
-        echo "credentials file does not exist."
-    fi
 
-    # Check if the Docker container ran successfully
-    if [ $? -eq 0 ]; then
-        echo "Container ran successfully. and push to dockerhub"
-        # docker tag ${IMAGE_NAME}:${IMAGE_TAG} tuzikexin/${IMAGE_NAME}:${IMAGE_TAG}
-        # docker login
-        docker push tuzikexin/${IMAGE_NAME}:${IMAGE_TAG}
+        # Check if the Docker container ran successfully
+        if [ $? -eq 0 ]; then
+            echo "Container ran successfully. and push to dockerhub"
+            # docker tag ${IMAGE_NAME}:${IMAGE_TAG} tuzikexin/${IMAGE_NAME}:${IMAGE_TAG}
+            # docker login
+            docker push tuzikexin/${IMAGE_NAME}:${IMAGE_TAG}
 
-        # Remove Dangling Images
-        docker system prune -f
-    else
-        echo "Container encountered an error during execution."
-        docker rm daily-vis-container
+            # Remove Dangling Images
+            docker system prune -f
+        else
+            echo "Container encountered an error during execution."
+            docker rm daily-vis-container
+        fi
     fi
 else
     echo "Docker image build failed."
--- daily_run_docker.sh
+++ daily_run_docker.sh
@@ -13,17 +13,17 @@ if [ $? -eq 0 ]; then
     credentials_folder="$current_dir/$credentials_f"
     echo 'credentials file path:' $credentials_folder
 
-    if [ -f $credentials_folder ]; then
+    if [ $(ls -A $credentials_folder | wc -l) -eq 0 ]; then
+        echo "credentials directory is empty."
+    else
         docker run --rm -v $credentials_folder:/app/credentials/ --name daily-vis-container tuzikexin/${IMAGE_NAME}:${IMAGE_TAG}  --ticker VIX --end_time_h 22 --end_time_m 45 --test_mode no
-    else
-        echo "credentials file does not exist."
-    fi
-    
-    # Check if the Docker container ran successfully
-    if [ $? -eq 0 ]; then
-        echo "Container ran successfully."
-    else
-        echo "Container encountered an error during execution."
-        docker rm daily-vis-container
+
+        # Check if the Docker container ran successfully
+        if [ $? -eq 0 ]; then
+            echo "Container ran successfully."
+        else
+            echo "Container encountered an error during execution."
+            docker rm daily-vis-container
+        fi
     fi
 fi
