3a736b9510db1b74d7f4d71aeab877883c1d2914
student125 <student125_9e556a7569414119@code.jdcloud.com>
2024-03-13 20:33:48 +0100

    restart time

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This project aiming for use cloud VM run a docker image download yahoo vix data 
 
 ### 
 1. login in the cloud VM https://orcaterm.cloud.tencent.com/
-~~~ 
+
 # run as root user
 sudo su -
 
@@ -25,12 +25,13 @@ mkdir credentials  # credentials.json upload to here
 
 # crontab for daily job
 crontab -e
-    # if the Docker container down, restart it
-    */5 * * * 1-5 raw_source_path/daily_restart_docker.sh >> /tmp/cron_test.log 2>&1
-    
-    # Start the Docker container at NY 8:55 AM on weekdays
-    55 20 * * 1-5 raw_source_path/daily_run_docker.sh >> /tmp/cron_test.log 2>&1
+~~~
+# if the Docker container down, restart it
+*/2 * * * 1-5 /home/lighthouse/Options_Auto/daily_restart_docker.sh >> /tmp/cron_test.log 2>&1
 
-    # Stop the Docker container at NY 16:05 PM on weekdays
-    5 4 * * 1-5 raw_source_path/daily_stop_docker.sh >> /tmp/cron_test.log 2>&1
+# Start the Docker container at NY 8:55 AM on weekdays
+55 20 * * 1-5 /home/lighthouse/Options_Auto/daily_run_docker.sh >> /tmp/cron_test.log 2>&1
+
+# Stop the Docker container at NY 16:05 PM on weekday
+5 4 * * 1-5 /home/lighthouse/Options_Auto/daily_stop_docker.sh >> /tmp/cron_test.log 2&1
 ~~~
--- daily_restart_docker.sh
+++ daily_restart_docker.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-start_hour=21   # this the beijing time
-start_minute=28
-end_hour=4
-end_minute=5
+# Function to convert time to minutes since midnight
+time_to_minutes() {
+    hour=$(echo $1 | cut -d: -f1)
+    minute=$(echo $1 | cut -d: -f2)
+    echo $((hour * 60 + minute))
+}
 
-# Get the current hour and minute
-current_hour=$(date +'%H')  # Current hour in 24-hour format
-current_minute=$(date +'%M')  # Current minute
+# Get current time and date
+current_time=$(date +%H:%M)
+current_minutes=$(time_to_minutes $current_time)
 
-# Convert times to minutes for easier comparison
-start_time_minutes=$((start_hour * 60 + start_minute))
-end_time_minutes=$((end_hour * 60 + end_minute))
-current_time_minutes=$((10#$current_hour * 60 + 10#$current_minute))  # Force base 10
+# Define time range
+start_time="20:55"  # this is beijing time 
+end_time="04:03"
 
-# Check if the current time is within the desired range
-if [ "$current_time_minutes" -ge "$start_time_minutes" ] && [ "$current_time_minutes" -le "$end_time_minutes" ]; then
-    # echo "Current time is within the range."
+# Convert range to minutes since midnight
+start_minutes=$(time_to_minutes $start_time)
+end_minutes=$(time_to_minutes $end_time)
+
+# Adjust for range that crosses midnight
+if [[ $current_minutes -lt $end_minutes ]]; then
+    current_minutes=$((current_minutes + 1440)) # Add 24 hours in minutes
+fi
+
+# Check if current time is within the range
+if [[ $current_minutes -ge $start_minutes ]] || [[ $current_minutes -lt $((end_minutes + 1440)) ]]; then
 
     # Check if the "option_download" container is running
     if ! docker ps | grep -q "daily-vis-container"; then
